18fe1e39082b615072591130ac816ff7922a5cc7
Katherine Kerr <katherinekerr@protonmail.com>
2019-07-31 12:11:09 +0100

    adds abstract car class

--- src/main/java/cars/Car.java
+++ src/main/java/cars/Car.java
@@ -1,4 +1,48 @@
 package cars;
 
-public class Car {
+import components.Doors;
+import components.Engine;
+import components.Tyres;
+
+public abstract class Car {
+
+    private Doors doors;
+    private Engine engine;
+    private Tyres tyres;
+    private String name;
+    private String colour;
+    private int price;
+
+    public Car(Doors doors, Engine engine, Tyres tyres, String name, String colour, int price) {
+        this.doors = doors;
+        this.engine = engine;
+        this.tyres = tyres;
+        this.name = name;
+        this.colour = colour;
+        this.price = price;
+    }
+
+    public Doors getDoors() {
+        return doors;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public Tyres getTyres() {
+        return tyres;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
